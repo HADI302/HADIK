a81194c595f620a44e856bf60ffff2e7f0b07cd3
HADI302 <74111007+HADI302@users.noreply.github.com>
2020-11-08 00:41:43 +0500

    Create README.py

---
+++ README.py
@@ -0,0 +1,15 @@
+Cloning into 'Fb-Crack'...
+remote: Enumerating objects: 105, done.
+remote: Counting objects: 100% (105/105), done.
+remote: Compressing objects: 100% (98/98), done.
+Receiving objects:  27% (29/105), 20.00 KiB | Receiving objects:  28% (30/105), 20.00 KiB | Receiving objects:  29% (31/105), 20.00 KiB | Receiving objects:  30% (32/105), 20.00 KiB | Receiving objects:  31% (33/105), 20.00 KiB | Receiving objects:  32% (34/105), 20.00 KiB | Receiving objects:  33% (35/105), 20.00 KiB | Receiving objects:  34% (36/105), 20.00 KiB | Receiving objects:  35% (37/105), 20.00 KiB | Receiving objects:  36% (38/105), 20.00 KiB | Receiving objects:  37% (39/105), 20.00 KiB | Receiving objects:  38% (40/105), 20.00 KiB | Receiving objects:  39% (41/105), 20.00 KiB | Receiving objects:  40% (42/105), 20.00 KiB | Receiving objects:  40% (43/105), 36.00 KiB
+| Receiving objects:  40% (43/105), 52.00 KiB | Receiving objects:  40% (43/105), 60.00 KiB |
+
+
+
+THIS MAY TAKE A WHILE, PLEASE BE PATIENT WHILE __ IS RUNNING...                             [▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+KHA
+[⊱⋄⊰]  Enter Username ➤HADI Enter Passward ➤KHAN
+
+
